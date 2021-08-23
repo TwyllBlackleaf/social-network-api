2928bb74a048f838982ea252d996a1641397c2ea
Twyll <twyllblackleaf@gmail.com>
2021-08-22 21:38:59 -0400

    added CRUD operations for thoughts to thought-controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,69 @@
+const { Thought } = require("../models");
+
+const thoughtController = {
+    // GET all thoughts
+    getAllThoughts(req, res) {
+        Thought.find()
+            .select("-__v")
+            .then(thoughtData => res.json(thoughtData))
+            .catch(err => res.json(err));
+    },
+
+    // GET single thought by _id
+    getThoughtById({ params }, res) {
+        Thought.findOne(
+            { _id: params.id }
+        )
+            .select("-__v")
+            .then(thoughtData => res.json(thoughtData))
+            .catch(err => res.json(err));
+    },
+
+    // POST new thought
+    createThought({ body }, res) {
+        Thought.create(body)
+            .then(({ _id }) => {
+                // push thought's id to associated user's thoughts array
+                return User.findOneAndUpdate(
+                    { _id: body.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                )
+            })
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: "No user found with this ID." })
+                }
+                res.json(userData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // PUT to update thought by _id
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { id: params.thoughtId },
+            body,
+            { new: true }
+        )
+            .then(thoughtData => {
+                if (!thoughtData) {
+                    res.status(404).json({ message: "No thought found with this ID." })
+                }
+                res.json(thoughtData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // DELETE thought by _id
+    deleteThought({params}, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+            .then(thoughtData => {
+                if (!thoughtData) {
+                    res.status(404).json({ message: "No thought found with this ID." })
+                }
+                res.json(thoughtData);
+            })
+            .catch(err => res.json(err));
+    }
+}
---
+++ controllers/user-controller.js
@@ -0,0 +1,14 @@
+const { User } = require("../models");
+
+const userController = {
+    // GET all users
+
+    // GET single user by _id
+    // Populate thought and friend data
+    
+    // POST new user
+
+    // PUT to update user by _id
+
+    // DELETE user by _id
+}
