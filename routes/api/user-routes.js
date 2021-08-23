5ddb43ad3569e355752af02dfc1451eaac3beca5
Twyll <twyllblackleaf@gmail.com>
2021-08-23 00:12:00 -0400

    fill out user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -8,3 +8,18 @@ const {
     addFriend,
     deleteFriend
 } = require("../../controllers/user-controller");
+
+router.route("/")
+    .get(getAllUsers)
+    .post(createUser);
+
+router.route("/:userId")
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router.route("/:userId/friends/:friendId")
+    .post(addFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
